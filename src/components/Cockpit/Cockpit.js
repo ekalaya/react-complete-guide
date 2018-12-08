fe62cabcc2ba681360ef08c3378083a8b8c86aa3
ekalaya <ekalaya@rocketmail.com>
2018-12-08 09:47:01 +0700

    Learn Fragment

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import clasess from './Cockpit.css'
+import clasess from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 const cockpit = (props) => {
     //let clasess = ['red','bold'].join(" ");
     const assignedClasess = [];
-    let btnClass = '';
+    let btnClass = clasess.Button;
 
     if(props.showPersons){
-        btnClass = clasess.Red;
+        btnClass = [clasess.Button, clasess.Red].join(' ');
     }
     
     if(props.persons.length <= 2){
@@ -18,12 +19,12 @@ const cockpit = (props) => {
     }
     
     return (
-        <div>
+        <Aux>
           <h1>{props.appTitle}</h1>
           <p className={assignedClasess.join(" ")}>This is really working!!!</p>
           <button className={btnClass}
           onClick={props.clicked}>Toggle Persons</button>
-       </div>
+       </Aux>
       );
 };
 
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,7 +9,12 @@ class Person extends Component {
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.change} value={this.props.name}></input>
             </div>
-            )
+        )
+           // return [
+             //   <p key="1" onClick={this.props.click}>I'm {this.props.name} and i'm {this.props.age} years old.</p>,
+               // <p key="2">{this.props.children}</p>,
+                //<input key="3" type="text" onChange={this.props.change} value={this.props.name}></input>
+            //]   
     }
 }
 
